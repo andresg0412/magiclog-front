f4c6e7c7b0880257b99115fbad3c5ec51825333c
German Andres Gamboa Portilla <andres.gamboa.9504@gmail.com>
2024-08-16 10:11:20 -0500

    New: Estructura base, Pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/Home/HomePage';
+import LoginPage from './pages/Login/LoginPage';
+import AdminPage from './pages/Admin/AdminPage';
+import VendorPage from './pages/Vendor/VendorPage';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/admin" element={<AdminPage />} />
+        <Route path="/vendor" element={<VendorPage />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/Vendor/VendorPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const VendorPage = () => {
+    return (
+        <div>
+            <h1>Vendedor Page</h1>
+        </div>
+    );
+};
+
+export default VendorPage;
