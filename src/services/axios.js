c7bcd2902f145e44deb9c500cc3b6c78fea2c1a2
German Andres Gamboa Portilla <andres.gamboa.9504@gmail.com>
2024-08-18 07:12:49 -0500

    Configuracion adicional para el error de CORS

--- src/services/axios.js
+++ src/services/axios.js
@@ -3,6 +3,9 @@ import axios from 'axios';
 const instance = axios.create({
     //baseURL: 'http://localhost:5000/api',
     baseURL: 'https://backend-magiclog.onrender.com/api',
+    headers: {
+        'Content-Type': 'application/json',
+    },
     withCredentials: true,
 });
 
